102f67faecdc8c64e356174c109f098a072634e9
Yureka Adhikari <adhikariyureka@gmail.com>
2024-11-08 09:39:35 +0545

    first commit

---
+++ data types.py
@@ -0,0 +1,15 @@
+name= input("What is your name: ")
+print(name)
+print(type(name))
+
+age= int(input("what is your age: "))
+print(age)
+print(type(age))
+
+weight= float(input("what is your weight: "))
+print(weight)
+print(type(weight))
+
+is_a_student= bool(input("Are you a student? [True or False]: "))
+print(is_a_student)
+print(type(is_a_student))
---
+++ string operation.py
@@ -0,0 +1,13 @@
+print("\033c")
+
+text = input("enter a string: ")
+
+revText= text[::-1]
+
+print(f"Original statement: {text}")
+
+print(f"\nUpperCase: {text.upper()}")
+
+print(f"\nLowerCase: {text.lower()}")
+
+print(f"\nReverse statement: {revText}")
